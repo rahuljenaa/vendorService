1e286b9c9d20b7583ef5ba5bf664ff2fe5b39c41
rahuljenaa <rahuljena1207@gmail.com>
2026-09-01 23:21:42 +0530

    Fix VendorController to inject VendorService interface for test compatibility

--- src/main/java/com/training/controller/VendorController.java
+++ src/main/java/com/training/controller/VendorController.java
@@ -1,18 +1,20 @@
 package com.training.controller;
 
-import com.training.business.bean.VendorBean;
-import com.training.service.VendorServiceImpl;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.training.business.bean.VendorBean;
+import com.training.service.VendorService;
 
 @RestController
 public class VendorController {
+
     @Autowired
-    private VendorServiceImpl vendorService;
+    private VendorService vendorService;
 
     @GetMapping("/")
     public String index() {
@@ -20,9 +22,8 @@ public class VendorController {
     }
 
     @GetMapping("/vendor/controller/getVendors")
-    public ResponseEntity<List<VendorBean>> getVendorDetails(){
+    public ResponseEntity<List<VendorBean>> getVendorDetails() {
         List<VendorBean> list = vendorService.getVendorDetails();
-
         return ResponseEntity.ok(list);
     }
 }
